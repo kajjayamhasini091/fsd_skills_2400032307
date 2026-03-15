82e8948e0504c9d13ebba530cf0a10e7a17bb00b
kajjayamHasini <hasini281206@gmail.com>
2026-03-15 17:03:43 +0530

    Added Spring MVC Web Request Handling Demo project

---
+++ SpringBootCourseAPI/src/main/java/com/klef/exception/demo/SpringBootCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.klef.exception.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseApiApplication.class, args);
+	}
+
+}
